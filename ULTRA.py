17fc6cfbae48d4462658bb8a5765899aa5ef172c
FB-KING-UPDATE-MASTER-PROGRAMMING <115982417+FB-KING-UPDATE-MASTER-PROGRAMMING@users.noreply.github.com>
2023-02-23 21:23:48 +0600

    Create ULTRA.py

---
+++ ULTRA.py
@@ -0,0 +1,29 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/FB.KING.MAHIN.NAME.TOH.SONSO')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from cr import CX
+ 
+        CX()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print ('32 Bit Not Allow Bye Bye Idol 😁')
